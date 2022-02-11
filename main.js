511310c7a6b0dc9fb24c657cac26fc087caaa258
ElJefe111944 <jamesfarr1@sky.com>
2022-02-11 15:24:56 +0000

    Afor loop has been added to loop through theobjects that represent each book. Embedded for loop has also been used to loop through arrays for authors. When the loop has finished the new html is added to the index page using the innerhtml property. Book titles, images, authors, page count and decription are now desplayed in the broweser inside a bootstrap card

--- main.js
+++ main.js
@@ -10,6 +10,39 @@ xhr.onload = function(){
     var newContent = ''; // Empty string for innerHTML to be pased through
     // newContent += '<p>' + responseObject.items[0].volumeInfo.title + '</p>';  // Test to ensure the first array object is being picked up
 
+    // for loop to loop through each book 
+    for (let i = 0; i < responseObject.items.length -2; i++){
+        newContent += '<div class="card mb-3" id="card">';
+		newContent += '<div class="row g-0">';
+		newContent += '<div class="col-4">';
+		// image
+		newContent += '<img src="' + responseObject.items[i].volumeInfo.imageLinks.smallThumbnail + '" class="img-fluid rounded-start" alt="...">';
+		newContent += '</div>';
+		newContent += '<div class="col-8">';
+		newContent += '<div class="card-body">';
+		// title
+		newContent += '<h6 class="card-title">' + responseObject.items[i].volumeInfo.title + '</h6>';
+		// Subtitle 
+		var subtitle = responseObject.items[i].volumeInfo.subtitle;
+		if (subtitle != undefined) {
+			newContent += '<h7 class="card-title">' + responseObject.items[i].volumeInfo.subtitle + '</h7>';
+		}
+		// for loop for authors
+		newContent += '<span class="card-text"><small class="text-muted">Author(s): </small></span> ';
+		for (let ii = 0; ii < responseObject.items[i].volumeInfo.authors.length; ii++) {
+			newContent += '<span class="card-text"><small class="text-muted">' + responseObject.items[i].volumeInfo.authors[ii] + '</small></span> ';
+		}
+		// page count
+		newContent += '<p class="card-text"><small>Number of pages: ' + responseObject.items[0].volumeInfo.pageCount + '</small></p>';
+		// description     
+		newContent += '<p class="card-text">' + responseObject.items[i].volumeInfo.description.substring(0, 140) + "...." + '</p>';
+
+		newContent += '</div>';
+		newContent += '</div>';
+		newContent += '</div>';
+		newContent += '</div>';
+    };
+
     document.getElementById("books").innerHTML = newContent;  // Update index page with new content
 
 
