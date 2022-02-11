42048fc5bb8c813e6e44e5b2a88db283151b8681
ElJefe111944 <jamesfarr1@sky.com>
2022-02-11 15:13:57 +0000

    The variable newContent has been created to hold the new HTML data. This has been linkjed to the targetted id books on the index page

--- main.js
+++ main.js
@@ -3,7 +3,17 @@ var xhr = new XMLHttpRequest();  // Create XMLHttpRequest object
 xhr.onload = function(){
     // console.log(xhr.responseText);  // Test to verify that API has been retrived
     var responseObject = JSON.parse(xhr.responseText);  // Convert JSON data from string to a javascript object 
+
+    // Features
+
+    // All books
+    var newContent = ''; // Empty string for innerHTML to be pased through
+
+    document.getElementById("books").innerHTML = newContent;  // Update index page with new content
+
+
+
 };
 
-xhr.open("GET", "https://www.googleapis.com/books/v1/volumes?q=HTML5");
-xhr.send(null);
+xhr.open("GET", "https://www.googleapis.com/books/v1/volumes?q=HTML5");  // Prepare request
+xhr.send(null);  // Send request
