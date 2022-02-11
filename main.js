b89df45ada93a11cc7e04ccfeb626ff8d8f7ea3b
ElJefe111944 <jamesfarr1@sky.com>
2022-02-11 15:02:26 +0000

    XMLHttpRequest object created to retrieve API JSON data and store in a variable called ResponseText. ResponseText then logged to console to verify that the JSON data has been retrieved. Test passed

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("Test passed");
+var xhr = new XMLHttpRequest();  // Create XMLHttpRequest object
+
+xhr.onload = function(){
+    console.log(xhr.responseText);
+};
+
+xhr.open("GET", "https://www.googleapis.com/books/v1/volumes?q=HTML5");
+xhr.send(null);
