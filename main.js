259c858e5bc63cdffeeacff06fd6be3df955e579
ElJefe111944 <jamesfarr1@sky.com>
2022-02-11 15:06:55 +0000

    Data stored in responseObject from XMLHttpRequest is converted from a string to a Javascript object using the pase method.

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 var xhr = new XMLHttpRequest();  // Create XMLHttpRequest object
 
 xhr.onload = function(){
-    console.log(xhr.responseText);
+    // console.log(xhr.responseText);  // Test to verify that API has been retrived
+    var responseObject = JSON.parse(xhr.responseText);  // Convert JSON data from string to a javascript object 
 };
 
 xhr.open("GET", "https://www.googleapis.com/books/v1/volumes?q=HTML5");
