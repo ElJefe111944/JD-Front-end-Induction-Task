234919bc26e5cfe2c6c8652a4c1cba17d6368386
ElJefe111944 <jamesfarr1@sky.com>
2022-02-13 21:39:24 +0000

    The description for each book in the feature section has now been added. This has been restricted to 140 characters. Also issue with images not rendering has now been rectified. The problem was a missing bracket to close the parent div

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function renderFeatures(responseObject){
 
 	for(let i = 0; i < responseObject.items.length; i++){
 		if(i >= responseObject.items.length - 2){
-			newFeatures += '<div class="card features"';
+			newFeatures += '<div class="card features">';
 			newFeatures += '<img src="' + responseObject.items[i].volumeInfo.imageLinks.smallThumbnail + '" class="card-img-top" alt="...">';
 			newFeatures += '<div class="card-body">';
 			newFeatures += '<h6 class="card-title">' + responseObject.items[i].volumeInfo.title + '</h6>';
@@ -31,6 +31,13 @@ function renderFeatures(responseObject){
 			}
 			// page count                
 			newFeatures += '<p class="card-text"><small>Number of pages: ' + responseObject.items[i].volumeInfo.pageCount + '</small></p>';
+			// decription
+			// Substring used to restrict the description to 140 characters
+			newFeatures += '<p class="card-text">' + responseObject.items[i].volumeInfo.description.substring(0, 140) + "...." + '</p>';
+			newFeatures += '</div>';
+			newFeatures += '</div>';
+			newFeatures += '</div>';
+
 
 
 		};
