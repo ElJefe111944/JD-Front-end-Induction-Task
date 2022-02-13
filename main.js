104baae88df4801989404bc094537a2ff3fd6d60
ElJefe111944 <jamesfarr1@sky.com>
2022-02-13 20:47:01 +0000

    All book items in for loop have been placed inside a function called rederItems to ensure cleaner and more readable code

--- main.js
+++ main.js
@@ -3,15 +3,20 @@ var xhr = new XMLHttpRequest();  // Create XMLHttpRequest object
 xhr.onload = function(){
     // console.log(xhr.responseText);  // Test to verify that API has been retrived
     var responseObject = JSON.parse(xhr.responseText);  // Convert JSON data from string to a javascript object 
+	renderItems(responseObject);  // All books rendered to html template
+
+};
 
     // Features
 
-    // All books
+function renderItems(responseObject){
+	// All books
     var newContent = ''; // Empty string for innerHTML to be pased through
     // newContent += '<p>' + responseObject.items[0].volumeInfo.title + '</p>';  // Test to ensure the first array object is being picked up
 
     // for loop to loop through each book 
     for (let i = 0; i < responseObject.items.length -2; i++){
+        // Start of card 
         newContent += '<div class="card mb-3" id="card">';
 		newContent += '<div class="row g-0">';
 		newContent += '<div class="col-4">';
@@ -41,13 +46,13 @@ xhr.onload = function(){
 		newContent += '</div>';
 		newContent += '</div>';
 		newContent += '</div>';
-    };
+	}
 
     document.getElementById("books").innerHTML = newContent;  // Update index page with new content
 
+}
 
-
-};
+    
 
 xhr.open("GET", "https://www.googleapis.com/books/v1/volumes?q=HTML5");  // Prepare request
 xhr.send(null);  // Send request
